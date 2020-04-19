75736be92ff2acfe43512bf079734e4ef9215331
Paulo <paulo.amigoni@gmail.com>
2020-04-19 13:11:17 -0300

    Test

---
+++ README.md
@@ -0,0 +1,4 @@
+## Desafio 06: Banco de dados e upload de arquivos no Node.js
+![](/imagens/topo.png)
+![](/imagens/1.png)
+![](/imagens/2.png)
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,25 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = this.find();
+
+    const income = (await transactions)
+      .filter(transaction => transaction.type === 'income')
+      .reduce((sum, actual) => sum + actual.value, 0);
+
+    const outcome = (await transactions)
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce((sum, actual) => sum + actual.value, 0);
+
+    const total = income - outcome;
+
+    const balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 }
 
